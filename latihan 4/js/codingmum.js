dfabb231a98e8fc90f017ea4c4aa935fa4dfc262
codingsarah <dsain.sarah@gmail.com>
2016-08-09 00:33:02 -0700

    TAMBAHAN
    JAVA SCRIPT

--- latihan 4/js/codingmum.js
+++ latihan 4/js/codingmum.js
@@ -11,14 +11,31 @@
 	- diskon fixed 10%
 
 	*/
-		function hitungJumlahHarga( jumlah ) {
+	function hitungJumlahHarga( jumlah ) {
 
-			var hasil;
-			var total;
+		var hasil;
+		var total;
 			total = jumlah * 2500;
 			hasil = total - (total*0.1)
 
 			return  hasil;
 
+}
 
-		}
+	function hitungJumlahHargaDiscountBersyarat(jumlah) {
+		
+	var nilai = 65;
+	var hasil;
+		total = jumlah * 2500;
+
+	if( total >= 25000) {
+	 	hasil = total - (total * 0.1);
+} else {
+		hasil = total;
+
+	}
+
+return hasil;
+
+}
+
